7a0c6255a0d709863034663154681b8f2ab3a7ad
m4nzm333 <irman.mashuri@gmail.com>
2024-12-30 06:05:33 +0800

    feat: add new method for repository and mapper

--- src/core/Mapper.ts
+++ src/core/Mapper.ts
@@ -1,4 +1,5 @@
 export interface Mapper<T> {
+  toPersistence(entity: T): any;
   toEntity(obj: any): T;
   toObject(entity: T): any;
 }
--- src/core/Repository.ts
+++ src/core/Repository.ts
@@ -2,4 +2,6 @@ export interface Repository<T> {
   save(data: T): Promise<T>;
   find(id: string): Promise<T>;
   findOne(query: any): Promise<T>;
+  findByQuery(query: any): Promise<Array<T>>;
+  delete(id: string): Promise<T>;
 }
