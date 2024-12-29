04bbc5ba4c7eee2e14e18d95c17a52a20c93bd9b
m4nzm333 <irman.mashuri@gmail.com>
2024-12-30 06:11:27 +0800

    feat: add null type for repository when returning data

--- src/core/Repository.ts
+++ src/core/Repository.ts
@@ -1,7 +1,7 @@
 export interface Repository<T> {
   save(data: T): Promise<T>;
-  find(id: string): Promise<T>;
-  findOne(query: any): Promise<T>;
-  findByQuery(query: any): Promise<Array<T>>;
-  delete(id: string): Promise<T>;
+  find(id: string): Promise<T | null>;
+  findOne(query: any): Promise<T | null>;
+  findByQuery(query: any): Promise<Array<T> | []>;
+  delete(id: string): Promise<T | null>;
 }
